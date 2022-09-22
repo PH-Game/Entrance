13c7939fd339a13c0074d69ad127ea1d803b948d
Deuter1um <alex-zjy@outlook.com>
2022-09-22 18:37:34 +0800

    feat: camera follow & hover

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,16 @@ public class PlayerController : MonoBehaviour
     public float runSpeed;
     private Rigidbody2D playerRigidbody;
     private Animator playerAnimator;
-    public int jumpRestTimes;
+    private int jumpRestTimes;
     public float JumpSpeed;
     public int jumpMaxTimes;
     private BoxCollider2D playerBoxCollider;
     private LayerMask ground;
-    private bool isGround;
     public float dashCD, dashSpeed, dashLen;
     private float lastDash;
+    public float hoverCD, hoverSpeed, hoverLen, JumpstartSpeed;
+    private float lastHover, hoverStart;
+    private bool isHovering;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +26,9 @@ public class PlayerController : MonoBehaviour
         playerBoxCollider = GetComponent<BoxCollider2D>();
         ground = LayerMask.GetMask("Ground");
         jumpRestTimes = jumpMaxTimes;
-        lastDash = (float)-1e6;
+        lastDash = (float) -1e6;
+        lastHover = (float) -1e6;
+        isHovering = false;
     }
 
     // Update is called once per frame
@@ -34,16 +38,17 @@ public class PlayerController : MonoBehaviour
         Flip();
         Jump();
         Dash();
+        Hover();
     }
 
-    void CheckGround()
+    bool CheckGround()
     {
-        isGround =  playerBoxCollider.IsTouchingLayers(ground);
+        return playerBoxCollider.IsTouchingLayers(ground);
     }
 
     void Run()
     {
-        if(Time.time - lastDash > dashLen)
+        if(Time.time - lastDash > dashLen && !isHovering)
         {
             float moveDir = Input.GetAxis("Horizontal");
             Vector2 playerVel = new Vector2(moveDir * runSpeed, playerRigidbody.velocity.y);
@@ -71,15 +76,12 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") || Input.GetKeyDown("w"))
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown("w")) && !isHovering)
         {
-            CheckGround();
-
-            if (isGround)
+            if (CheckGround())
             {
                 jumpRestTimes = jumpMaxTimes;
             }
-
             if (jumpRestTimes != 0)
             {
                 Vector2 jumpVel = new Vector2(0.0f, JumpSpeed);
@@ -110,8 +112,37 @@ public class PlayerController : MonoBehaviour
        }
        if(Time.time - lastDash < dashLen)
        {
-            Vector2 playerVel = new Vector2(dashSpeed * FacingDir(), playerRigidbody.velocity.y);
+            Vector2 playerVel = new Vector2(dashSpeed * FacingDir(), 0);
             playerRigidbody.velocity = playerVel;
        }
+    }
+
+    void Hover()
+    {
+        if(Input.GetKeyDown("c") && Time.time - lastHover > hoverCD)
+        {
+            isHovering = true;
+            hoverStart = Time.time;
+            if(CheckGround())
+            {
+                Vector2 playerVel = new Vector2(0, JumpstartSpeed);
+                playerRigidbody.velocity = Vector2.up * playerVel;
+            }
+        }
+        if(isHovering && Time.time - hoverStart < hoverLen && Time.time - hoverStart > 0.1)
+        {
+            lastHover = Time.time;
+            if(CheckGround())
+            {
+                isHovering = false;
+            }
+            float xMoveDir = Input.GetAxis("Horizontal"), yMoveDir = Input.GetAxis("Vertical");
+            Vector2 playerVel = new Vector2(xMoveDir * hoverSpeed, yMoveDir * hoverSpeed * 0.5f);
+            playerRigidbody.velocity = playerVel;
+        }
+        if(Time.time - hoverStart > hoverLen)
+        {
+            isHovering = false;
+        }
     }
 }
